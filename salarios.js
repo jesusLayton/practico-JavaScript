f20174c7e6b6f457d03bc2a1ed26dc75256001fa
jesusLayton <jesuslayton92@gmail.com>
2022-04-10 16:28:46 -0500

    commit

---
+++ salarios.js
@@ -0,0 +1,121 @@
+const colombia = [];
+colombia.push(
+    {
+        name: "juanita",
+        salary: 800,
+    }
+);
+colombia.push(
+    {
+        name: "Natalia",
+        salary: 5000,
+    }
+);
+colombia.push(
+    {
+        name: "pepito",
+        salary: 3000,
+    }
+);
+colombia.push(
+    {
+        name: "carlos",
+        salary: 90000,
+    }
+);
+colombia.push(
+    {
+        name: "jose",
+        salary: 12000,
+    }
+);
+colombia.push(
+    {
+        name: "sebastian",
+        salary: 600,
+    }
+);
+colombia.push(
+    {
+        name: "nicolas",
+        salary: 8500,
+    }
+);
+colombia.push(
+    {
+        name: "abelardo",
+        salary: 9600,
+    }
+);
+colombia.push(
+    {
+        name: "elkin",
+        salary: 4400,
+    }
+);
+colombia.push(
+    {
+        name: "cesar",
+        salary: 15,
+    }
+);
+colombia.push(
+    {
+        name: "pablo",
+        salary: 8000,
+    }
+);
+colombia.push(
+    {
+        name: "ana",
+        salary: 10000,
+    }
+);
+colombia.push(
+    {
+        name: "angelica",
+        salary: 500,
+    }
+);
+colombia.push(
+    {
+        name: "berna",
+        salary: 3000,
+    }
+);
+colombia.push(
+    {
+        name: "nelson",
+        salary: 2000,
+    }
+);
+colombia.push(
+    {
+        name: "lalo",
+        salary: 4500,
+    }
+);
+colombia.push(
+    {
+        name: "hervert",
+        salary: 100,
+    }
+);
+colombia.push(
+    {
+        name: "nana",
+        salary: 10000,
+    }
+);
+colombia.push(
+    {
+        name: "myriam",
+        salary: 9000,
+    }
+);
+colombia.push(
+    {
+        name: "miguel",
+        salary: 8000,
+    }
+);
